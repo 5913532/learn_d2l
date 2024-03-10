7e352152ed0d15863ef4f9169c0df90e18b687dd
jiepengtan <jiepengtan@gmail.com>
2024-03-10 19:10:00 +0800

    update pull script

--- sync_config_pull.sh
+++ sync_config_pull.sh
@@ -5,4 +5,21 @@ if [ ! -d $dst_dir ]; then
   exit 1
 fi
 
+cd $dst_dir
+
+WORKDIR="../_remote_config"
+mkdir -p $WORKDIR
+cd $WORKDIR
+if [ ! -d ".git" ]; then
+    git clone https://github.com/JiepengTan/test_mb.git ./
+fi
 git pull origin main
+cd -
+
+dirs=("src" "pytorch" )
+
+for dir in "${dirs[@]}"; do
+  rm -rf "./$dir"
+  cp -rf "$WORKDIR/$dir" .
+done
+
