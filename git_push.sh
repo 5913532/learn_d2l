faeaf3f6dbaefe6a264aeee856917f21255a4f5a
jiepengtan <jiepengtan@gmail.com>
2024-03-16 10:55:58 +0800

    Auto commit: Sat Mar 16 10:55:58 CST 2024

--- git_push.sh
+++ git_push.sh
@@ -2,14 +2,11 @@
 dst_dir=/root/autodl-tmp/d2l
 if [ -d $dst_dir ]; then
   echo "The directory $dst_dir exist."
-  exit 1
 fi
 
 git add .
 git commit -m "Auto commit: $(date)"
 git push origin main --force
-
-
 
 
 
